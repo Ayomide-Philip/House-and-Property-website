0ee7839a0fd13b2aca9b840b67e7ae3188edd4f8
A-pilip <areoayomide2008@gmail.com>
2024-09-29 23:06:08 +0100

    updating my script

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const imageSlideArray = [
+  "./images/container-image/pexels-asadphoto-1268871.jpg",
+  "./images/container-image/pexels-atomlaborblog-1105754.jpg",
+  "./images/container-image/pexels-pok-rie-33563-28628302.jpg",
+  "./images/container-image/pexels-quang-nguyen-vinh-222549-2155202.jpg",
+];
+
+const imageSlide = document.getElementById("image-slide");
+
+setInterval(imageSlideAnimation, 3000);
+
+var i = 0;
+
+function imageSlideAnimation() {
+  imageSlide.setAttribute("src", imageSlideArray[i]);
+  i++;
+
+  if (i > imageSlideArray.length - 1) {
+    i = 0;
+  }
+}
